f841b364eeb82712cd26e918a1b5d53321192f09
anaaquirino <analuiza.qv2811@gmail.com>
2024-02-28 11:48:24 -0300

    calculadora

--- portfolio/js/calculadora-imc.js
+++ portfolio/js/calculadora-imc.js
@@ -20,10 +20,31 @@ formulario.addEventListener('submit' , function(e){ //a função aguarda o usuá
     const imc = calcularImc(peso, altura)
 
     console.log(imc)
+
+    const classificação = tabelaImc (imc) // estou chamando a função tabelaIMC
+    console.log(classificação)
+    mostrarResultado(classificação)
 })
 
 //função para calcular o IMC e retornar o valor do imc com duas casas decimais
 function calcularImc(peso, altura){
     let imc = (peso / altura ** 2); //fórmula do imc
     return imc.toFixed(2); //toFixed(2) foi usado para formatar o número em 2 casas decimais
+}
+
+//função para retornar o valor a tela
+function mostrarResultado(msg){
+    const resultado = document.querySelector("#resultado");
+    resultado.innerHTML = msg
+}
+
+//função para classificar em qual grau de imc a pessoa será
+function tabelaImc(imc){
+    const classificação = [ 'abaixo do peso' , 'peso normal' , 'sobrepeso' ,
+                            'obesidade grau I' , 'obesidade grau II' , 'obesidade grau III'] 
+if(imc >= 39.9) return classificação[5];
+if(imc >= 34.9) return classificação[4];
+if (imc >= 29.9) return classificação[3];
+if (imc >= 24.9) return classificação[2];
+if (imc >= 18.5) return classificação[1];
 }
