b6ca8bea7fb8431daef26e231a6537c0f967635c
anaaquirino <analuiza.qv2811@gmail.com>
2024-03-12 11:42:16 -0300

    slide automatico

---
+++ aula 12/js/slider-automatico.js
@@ -0,0 +1,22 @@
+var slideIndex = 0;
+
+carrosel()
+
+function carrosel(){
+    var x =document.getElementsByClassName("meuSlide")
+
+    for(var i = 0; i < x.length; i++ ){
+        x[i].style.display = "none"
+    }
+    slideIndex++;
+    if(slideIndex > x.length){
+        slideIndex = 1;
+
+    }
+
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carrosel,1000);
+
+
+    console.log(x)
+}
